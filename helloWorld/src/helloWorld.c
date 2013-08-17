d44fc85abfb677d2c27de51c49b68713543dc218
roneymonte <roneymonte@gmail.com>
2013-08-17 20:17:51 -0300

    primeiro nivel de sincronizacao

---
+++ helloWorld/src/helloWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : helloWorld.c
+ Author      : roney
+ Version     :
+ Copyright   : just a test
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
