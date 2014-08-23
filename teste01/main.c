755d9628882a82b13b1e2c61a8c4cdc19da1182b
roney <roneymonte@gmail.com>
2014-08-23 18:11:44 -0300

    teste01

---
+++ teste01/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: roney
+ *
+ * Created on 23 de Agosto de 2014, 18:11
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
