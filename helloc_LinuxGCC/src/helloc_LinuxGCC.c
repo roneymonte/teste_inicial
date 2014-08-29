a4693c79ba2d06ee57a1244ff0bd800ab4c7ec30
roneymonte <roneymonte@gmail.com>
2014-08-29 16:08:35 -0300

    teste de criacao de novo projeto, simples e rapido, usando Eclipse 4.4 e GIT

---
+++ helloc_LinuxGCC/src/helloc_LinuxGCC.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : helloc_LinuxGCC.c
+ Author      : roney
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+//#include <stddef.h>
+//#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
